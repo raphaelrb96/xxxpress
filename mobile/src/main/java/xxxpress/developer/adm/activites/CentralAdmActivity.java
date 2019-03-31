30a137fc99313adb022cdf57430945f8a6a445dc
Raphael Almeida <raphael.rb96@gmail.com>
2019-03-30 23:31:53 -0400

    ImplementacaoAdminCentral

---
+++ mobile/src/main/java/xxxpress/developer/adm/activites/CentralAdmActivity.java
@@ -0,0 +1,17 @@
+package xxxpress.developer.adm.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xxxpress.developer.R;
+
+public class CentralAdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central_adm);
+
+
+    }
+}
